547df150e3bc0f25f7c2536b95a5e3c250d49cd6
Meni Samet <meni432@gmail.com>
2016-04-28 15:44:54 +0300

    upload file

---
+++ OS-Server/src/ConnectionManager.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverPack;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ConnectionManager implements Runnable {
+// TODO change in the client to ObjectInputStream
+    
+    private final ReentrantLock lock;
+    ServerSocket socketS;
+    List<InOutStreams> streamList;
+    int currentStream = 0;
+
+    public ConnectionManager(ServerSocket socketS,ReentrantLock lock) {
+        this.socketS = socketS;
+        this.streamList=Collections.synchronizedList(new ArrayList<InOutStreams>());
+        this.lock=lock;
+    }
+
+    public void run() {
+        /// why do we need lock????
+        while (true) {
+            try {
+                Socket s = socketS.accept();
+                System.out.println("connection accept");
+              lock.lock();
+                try {
+                    streamList.add(new InOutStreams(s));
+                } finally {
+                    lock.unlock();
+                }
+            } catch (Exception e) {
+                System.out.println("problem in the seerver connection");
+            }
+        }
+    }
+
+    public List<InOutStreams> getStreamList() {
+        return streamList;
+    }
+
+ 
+
+}
---
+++ OS-Server/src/STask.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverPack;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ofir Arnon
+ */
+public class STask implements Runnable {
+
+    InOutStreams ois;
+    int query;
+    
+    public STask(InOutStreams ois, int query){
+        this.ois = ois;
+        this.query = query;
+    }
+    
+    @Override
+    public void run() {
+        
+        try {
+            Thread.sleep(20);
+            ois.getOos().writeObject(query);
+            System.out.println(query+" Sent");
+        } catch (IOException ex) {
+            try {
+                ois.getOis().close();
+                ois.getOos().close();
+                ois.getSocket().close();
+                System.err.println("STask colse connection(on write)");
+            } catch (IOException ex1) {
+                System.err.println("catch in STask");
+            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(STask.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
+
+
+//    ConnectionManager cm;
+//
+//    public STask(ConnectionManager cm) {
+//        this.cm = cm;
+//    }
+//    
+//    
+//
+//    @Override
+//    public void run() {
+//        int query;
+//        InOutStreams streams = cm.getStreams();
+//        if (streams == null) {
+//            // System.out.println("streams null");
+//            return;
+//        }
+//        try {
+//            //System.out.println("streams NOT null");
+//            if (streams.getOis().available() == 0) {
+//                System.out.println("available()==0");
+//                return;
+//            }
+//            System.out.println("available != 0");
+//            query = streams.getOis().readInt();
+//            System.out.println("query:" + query);
+//        } catch (IOException ex) {
+//            System.out.println("end of stream");
+//            return;
+//        }
+//        try {
+//            //-------------------------------------//
+//            // search in the cash
+//            //-------------------------------------//
+//            int ans = Database.readY(query);
+//            streams.getOos().writeInt(ans);
+//        } catch (IOException ex) {
+//            System.out.println("problem in reading");
+//            return;
+//        }
+//
+//    }
+    
