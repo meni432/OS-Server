60e38033332fa55da0eb82e0aa551940df00509d
Meni Samet <meni432@gmail.com>
2016-05-23 15:10:26 +0300

    make DbWriterThread and CashWriterThread as anonymous method

--- OS-Server/src/ServerPack/Server.java
+++ OS-Server/src/ServerPack/Server.java
@@ -22,18 +22,14 @@ public class Server {
     private static final ReentrantLock lock = new ReentrantLock(true);
     private static ServerSocket serverSocket;
     public static final int TIME_TO_WAIT = 1000; // time to wait for read object until interrupt him
-    
+
     public static final int S_THREADS_NUM = 5; //1. S - number of allowed S-threads.
     public static final int CASH_SIZE = 100; //C - size of the cache.
     public static final int LEAST_TO_CACHE = 10;//3. M - the least number of times a query has to be requested in order to be allowed to enter the cache.
     public static final int RANDOM_RANGE = 2; //4. L - to specify the range [1, L] from which missing replies will be drawn uniformly at random.
     public static final int NUMBER_OF_READER_THREADS = 7; //5. Y - number of reader threads.
-    
-    
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void creatsThreadPools() {
         seachersPool = new ThreadPool(S_THREADS_NUM, "seachersPool");
         cashReadersPool = new ThreadPool(NUMBER_OF_READER_THREADS, "cashReadersPool");
@@ -41,12 +37,28 @@ public class Server {
     }
 
     public static void startWriteThreads() {
-        new Thread(new DbWriterThread()).start();
-        new Thread(new CashWriteThread()).start();
+        new Thread("DbWriterThread") {
+            @Override
+            public void run() {
+                while (true) {
+                    DatabaseManager.writeAll();
+                    System.err.println("------------update DB completed---------------");
+                }
+            }
+        }.start();
+        new Thread("CashWriteThread") {
+            @Override
+            public void run() {
+                while (true) {
+                    CashManager.updateCash();
+                    System.err.println("------------update cash completed---------------");
+                }
+            }
+        }.start();
     }
 
     public static void startConnection() {
-        
+
         try {
             serverSocket = new ServerSocket(PORT);
             SyncArrayList<InOutStreams> streamList = new SyncArrayList<>();
