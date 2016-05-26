c17d5e2ec5cd0b136c07eb4e8751fd1c1334356b
Meni Samet <meni432@gmail.com>
2016-05-26 11:01:30 +0300

    Add a negative integer Mapping for DB

--- OS-Server/src/ServerPack/DatabaseManager.java
+++ OS-Server/src/ServerPack/DatabaseManager.java
@@ -1,6 +1,5 @@
 package ServerPack;
 
-
 import java.io.EOFException;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -14,31 +13,31 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * @author Meni Samet
+ * Management the writing and reading from file
  */
 public final class DatabaseManager {
-     /* a temporary structure to hold the elements who needs to be write to the DB */
+
+    /* a temporary structure to hold the elements who needs to be write to the DB */
     private static final HashMap<Integer, XYZ> elemToUpdateDB = new HashMap<>();
     final static int FILE_MAX_CAPACITY = 100; // FILE_MAX_CAPACITY (x,y,z) trio on each file
     final static int UPDATE_DB_REACHED = 100; // UPDATE_DB_REACHED 
     final static int OBJECT_SIZE = Integer.BYTES * 3; // trio (x,y,z) size in bytes
     final static int NOT_FOUND = -1; // indicate that the trio not found
     final static int INITIAL_VALUE = 1; // z initial value
-    /* when last update time minus current time bigger than TIME_TO_UPDATE_MS writer DB thread start update */    
+    /* when last update time minus current time bigger than TIME_TO_UPDATE_MS writer DB thread start update */
     private static final int TIME_TO_UPDATE_MS = 5000;
-     /* readWriteLock to synchronize between DB read-write action */   
+    /* readWriteLock to synchronize between DB read-write action */
     private static final ReadWriteLock readWriteLock = new ReadWriteLock();
-     /* semaphore that holds the writer DB thread until TIME_TO_UPDATE_MS or UPDATE_DB_REACHED reached */
+    /* semaphore that holds the writer DB thread until TIME_TO_UPDATE_MS or UPDATE_DB_REACHED reached */
     private static final Semaphore semUpdateDB = new Semaphore(0);
-    /* holds the last update DB time */    
+    /* holds the last update DB time */
     private static long LastUpdate = System.currentTimeMillis();
 
-    
     /**
-     * singleton design pattern 
+     * singleton design pattern
      */
     private DatabaseManager() {
-    } 
+    }
 
     /**
      * generate file name for DB
@@ -47,7 +46,12 @@ public final class DatabaseManager {
      * @return filename contain the result
      */
     private static String getFileName(int x) {
-        return "" + (x / FILE_MAX_CAPACITY) * FILE_MAX_CAPACITY + ".db";
+        if (x >= 0) { // in case that x is positive integer
+            return "" + (x / FILE_MAX_CAPACITY) * FILE_MAX_CAPACITY + ".db";
+        } else { // in case that x is negative integer, put in diffrent file
+            x = Math.abs(x);
+            return "" + (x / FILE_MAX_CAPACITY) * FILE_MAX_CAPACITY + "-NEG.db";
+        }
     }
 
     /**
@@ -55,6 +59,8 @@ public final class DatabaseManager {
      * @return position of the first byte
      */
     private static int getPosition(int x) {
+        // in file, whenever x is negative or positive, mapping like is possitive
+        x = Math.abs(x);
         return (x % (FILE_MAX_CAPACITY)) * OBJECT_SIZE;
     }
 
@@ -99,8 +105,9 @@ public final class DatabaseManager {
             }
         }
     }
+
     /**
-     * 
+     *
      * @param toPut trio XYZ to update z from cache to DB
      */
     public static void updateFromCash(XYZ toPut) {
@@ -113,11 +120,13 @@ public final class DatabaseManager {
             readWriteLock.unlockRead();
         }
     }
-/**
- * search query x in the DB 
- * @param query x 
- * @return y answer
- */
+
+    /**
+     * search query x in the DB
+     *
+     * @param query x
+     * @return y answer
+     */
     public static int readY(int query) {
 
         try {
@@ -127,7 +136,7 @@ public final class DatabaseManager {
             XYZ trio;
             if ((trio = elemToUpdateDB.get(query)) != null) { // if the trio founds in the elemToUpdateDB temporay structure
                 trio.incZ();
-                ans = trio.getY(); 
+                ans = trio.getY();
             } else { // search in the DB files
                 trio = readDBHelper(query);
                 ans = trio.getY();
@@ -142,7 +151,7 @@ public final class DatabaseManager {
                 toUpdate.incZ();
                 elemToUpdateDB.put(query, toUpdate); // put toUpdate in the temporary structure
             }
-            
+
             long diffTime = System.currentTimeMillis() - LastUpdate;
             if (elemToUpdateDB.size() >= UPDATE_DB_REACHED || diffTime > TIME_TO_UPDATE_MS) { // invoke DB writer Thread
                 semUpdateDB.release();
@@ -154,15 +163,15 @@ public final class DatabaseManager {
 
         } catch (InterruptedException ex) {
             ex.printStackTrace();
-             return NOT_FOUND;
+            return NOT_FOUND;
         } finally {
             readWriteLock.unlockRead();
-        }   
+        }
     }
 
-
     /**
-     *  write trio (x,y,z) to the DB files
+     * write trio (x,y,z) to the DB files
+     *
      * @param x query
      * @param y answer
      * @param incZ increment z in the given incZ
@@ -199,7 +208,7 @@ public final class DatabaseManager {
      *
      * @param x the request value
      * @param y answer
-     * @param newZ 
+     * @param newZ
      */
     private static void writeXYOvverideZ(int x, int y, int newZ) {
         RandomAccessFile raf = null;
@@ -225,9 +234,10 @@ public final class DatabaseManager {
             }
         }
     }
+
     /**
      * write all the elements from elemToUpdateDB to the DB
- 
+     *
      */
     public static void updateDB() {
         try {
@@ -258,7 +268,6 @@ public final class DatabaseManager {
             Logger.getLogger(DatabaseManager.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
 
     /**
      * only for test DB files
